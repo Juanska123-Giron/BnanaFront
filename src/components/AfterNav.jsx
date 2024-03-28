1b6a96c114a03623d02086c1247b4893b90c75a9
MaitreStick <mastijaci99@gmail.com>
2024-03-27 21:19:03 -0500

    se modificó la tabla con los parámetros y se creó un botón que habilita una paginación para evaluar el software, aún no se configura la paginación

--- src/components/AfterNav.jsx
+++ src/components/AfterNav.jsx
@@ -15,17 +15,52 @@ export const AfterNav = ({ isPopoverOpen }) => {
   const softwareItems = [
     {
       codigo: 1,
-      item: 'Software A',
-      descripcion: 'Descripción del Software A',
-      preguntas: 10,
-      porcentajeTotal: '90%'
+      item: 'FUNCIONALIDAD',
+      descripcion: 'La capacidad del software de cumplir con las funciones para satisfacer las necesidades explícitas e implícitas cuando es utilizado en condiciones específicas.',
+      preguntas: 5,
+      porcentajeTotal: '14%'
     },
     {
       codigo: 2,
-      item: 'Software B',
-      descripcion: 'Descripción del Software B',
-      preguntas: 8,
-      porcentajeTotal: '85%'
+      item: 'FIABILIDAD',
+      descripcion: 'La capacidad del software  para  asegurar  un  nivel   de funcionamiento adecuado cuando es  utilizando en condiciones especificas.',
+      preguntas: 4,
+      porcentajeTotal: '14%'
+    },
+    {
+      codigo: 3,
+      item: 'USABILIDAD',
+      descripcion: 'La capacidad del software de ser entendido, aprendido, y usado en forma fácil y atractiva',
+      preguntas: 5,
+      porcentajeTotal: '15%'
+    },
+    {
+      codigo: 4,
+      item: 'EFICIENCIA',
+      descripcion: 'La forma del desempeño adecuado, de acuerdo a al número  recursos utilizados según las condiciones planteadas',
+      preguntas: 3,
+      porcentajeTotal: '15%'
+    },
+    {
+      codigo: 5,
+      item: 'CAPACIDAD DE MANTENIMIENTO',
+      descripcion: 'La cualidad que tiene el software para ser modificado. Incluyendo correcciones o mejoras del  software, a cambios en el entorno, y especificaciones de requerimientos funcionales.',
+      preguntas: 5,
+      porcentajeTotal: '14%'
+    },
+    {
+      codigo: 6,
+      item: 'PORTABILIDAD',
+      descripcion: 'La capacidad que tiene el software para ser trasladado de un entorno a otro. cubriendo entornos organizacionales, de hardware o de software',
+      preguntas: 5,
+      porcentajeTotal: '14%'
+    },
+    {
+      codigo: 7,
+      item: 'CALIDAD EN USO',
+      descripcion: 'Capacidad del software para permitirles a usuarios lograr las  metas  propuestas  con  eficacia,  productividad,  seguridad  y  satisfacción,  en contextos especificados de uso',
+      preguntas: 6,
+      porcentajeTotal: '14%'
     },
   ];
 
@@ -33,7 +68,7 @@ export const AfterNav = ({ isPopoverOpen }) => {
     <div className={`relative overflow-hidden bg-white transition-all duration-500 ease-in-out ${isPopoverOpen ? 'mt-128' : 'mt-0'}`}>
       <div className={`relative overfxlow-hidden bg-white transition-all duration-500 ease-in-out pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40 ${isPopoverOpen ? 'mt-0' : 'mt-0'}`}>
         <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
-        <h2 className="text-xl font-semibold mb-4">Calificación de Software</h2>
+        <h2 className="text-xl font-semibold mb-4">PARÁMETROS</h2>
         <div className="overflow-x-auto">
           <table className="min-w-full border-collapse border border-gray-300">
             <thead className="bg-gray-100">
@@ -57,7 +92,12 @@ export const AfterNav = ({ isPopoverOpen }) => {
               ))}
             </tbody>
           </table>
-        </div>          
+        </div>
+        <div className="flex justify-center mt-8">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              EVALUAR SOFTWARE
+            </button>
+        </div>             
         </div>
       </div>
     </div>
