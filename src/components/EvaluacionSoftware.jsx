850064b2cf2d8886668297bdc26526de21a3c0f1
MaitreStick <mastijaci99@gmail.com>
2024-03-28 11:37:12 -0500

    actualización de componentes

--- src/components/EvaluacionSoftware.jsx
+++ src/components/EvaluacionSoftware.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import ComponenteTabla from './Tabla';
+import ComponenteTabla from './ComponenteTabla';
 
 import { datosFuncionalidad, datosFiabilidad, datosUsabilidad, datosEficiencia, datosCapacidadDeMantenimiento, datosPortabilidad, datosCalidadDeUso} from './datosSecciones';
 
@@ -33,7 +33,7 @@ const EvaluacionSoftware = () => {
 
   return (
     <>
-      <div className="flex justify-around mb-4">
+      <div className="flex justify-around mb-10 mt-10">
         <button onClick={() => cambiarSeccion('funcionalidad')} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-150 ease-in-out">
           Funcionalidad
         </button>
@@ -58,8 +58,8 @@ const EvaluacionSoftware = () => {
       </div>
       <ComponenteTabla datos={datosActuales} />
       <div className="flex justify-around mt-10">
-        <button onClick={volver} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-150 ease-in-out">Volver</button>
-        <button onClick={finalizarEvaluacion} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-150 ease-in-out">Finalizar Evaluación</button>
+        <button onClick={volver} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded transition duration-150 ease-in-out">Volver</button>
+        <button onClick={finalizarEvaluacion} className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-150 ease-in-out">Finalizar Evaluación</button>
       </div>
     </>
   );
